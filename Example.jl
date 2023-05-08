5964f72c9dcdff5d2ffccd4fbc81d71ce797470e
ZongYongyue <zongyyphy@qq.com>
2023-05-08 17:48:17 +0800

    add new functions

--- Example.jl
+++ Example.jl
@@ -3,34 +3,18 @@ using LinearAlgebra: Eigen
 using SparseArrays: SparseMatrixCSC, spzeros
 using QuantumLattices
 using ExactDiagonalization
-
-using Pkg;
-Pkg.add("/Users/tzung/Library/Mobile Documents/com~apple~CloudDocs/mygit/CPTVCA")
+using BlockArrays
+using Revise
 using CPTVCA
-#= 
-"""
-get  the matrix representation Hamiltonian of a single cluster systerm
-"""
-struct ClusterH{L<:AbstractLattice, G<:OperatorSum, M<:OperatorSum} <: Frontend
-    cluster::L
-    clH::G
-    clHₘ::M
-end
-function ClusterH(cluster::AbstractLattice, hilbert::Hilbert, terms::Tuple{Vararg{Term}}, targetspace::TargetSpace; neighbors::Union{Nothing, Int, Neighbors}=nothing, boundary::Boundary=plain)
-    ed = ED(cluster, hilbert, terms, targetspace; neighbors, boundary)
-    clH = expand(ed.H)
-    clHₘ = expand(ed.Hₘ)
-    return ClusterH(cluster, clH, clHₘ)
-end
-=#
-lattice = Lattice([0.0, 0.0], [1, 0])
-bond = bonds(lattice, Neighbors(1=>1.0))
-hilbert = Hilbert(site=>Fock{:f}(1, 2) for site=1:length(lattice))
-bases = BinaryBases(4, 2)
-coupling = Coupling(Index(:,FID{:f}(1,:,:)),Index(:,FID{:f}(1,:,:)))
-t = Hopping(:t, Complex(-1.0), 1, coupling)
-U = Hubbard(:U, Complex(4.0))
-H = ClusterH(lattice, hilbert, (t, U), TargetSpace(bases); boundary = plain)
 
-H.clH
-H.clHₘ
+lattice = Lattice([0, 0], [1, 0], [0,1], [1,1])
+
+cluster = Cluster(lattice)
+
+vectors = [[2,0],[0,2]]
+
+clusters = Clusters(cluster,(1,1),vectors)
+
+interlinks(cluster,clusters[8],Neighbors(1=>1.0))
+filter(x->x!=cluster,clusters)[8]
+typeof(interlinks(cluster,clusters[8],Neighbors(1=>1.0)))
--- src/CPTVCA.jl
+++ src/CPTVCA.jl
@@ -5,11 +5,63 @@ using LinearAlgebra: Eigen
 using SparseArrays: SparseMatrixCSC, spzeros
 using QuantumLattices
 using ExactDiagonalization
-
-export ClusterH
+using BlockArrays
+export ClusterH, Cluster, Clusters
+"""
+define the cluster of a quantum lattice systerm
+"""
+struct Cluster{T<:Number} <: AbstractMatrix{T}
+    intrasites::Matrix{T}
+    id::Int
+end
+Base.size(c::Cluster)=size(c.intrasites)
+Base.getindex(c::Cluster, i, j) = c.intrasites[i, j]
+Base.setindex!(c::Cluster, v, i, j) = (c.intrasites[i, j] = v)
+function Cluster(lattice::AbstractLattice, id::Int=0)
+    intrasites = lattice.coordinates
+    return Cluster(intrasites, id)
+end
 
 """
-get  the matrix representation Hamiltonian of a single cluster systerm
+get a supercluster consisting of a given cluster and its surrounding clusters 
+"""
+function Clusters(cluster::Cluster, steps::Tuple{Int, Vararg{Int}}, vectors::AbstractVector{<:AbstractVector{<:Number}})
+    @assert length(steps) == length(vectors) "steps and vectors must have the same length"
+    translations = Array{Array{Int64, 1}, 1}()
+    if length(steps) == 2
+        for i in -steps[1]:steps[1], j in -steps[2]:steps[2]
+            push!(translations, [i, j])
+        end
+    elseif length(steps) == 3
+        for i in -steps[1]:steps[1], j in -steps[2]:steps[2], k in -steps[3]:steps[3]
+            push!(translations, [i, j, k])
+        end
+    else
+        error("Dimension is limited to 2 or 3.")
+    end
+    supermatrix = tile(cluster, vectors, translations)
+    index = convert(Int, size(supermatrix,2)/size(cluster.intrasites,2))
+    supermatrix_block = BlockArray(supermatrix, [size(cluster.intrasites,1)],fill(size(cluster.intrasites,2),index))
+    clusters = Vector{Cluster}()
+    for i in 1:index
+        push!(clusters, Cluster(supermatrix_block[Block(1,i)],i))
+    end
+    return clusters
+end
+
+"""
+get the inter-cluster bonds of the given cluster and its surrounding clusters
+"""
+function interbonds(clusters::Vector{Cluster}, cluster::Cluster, neighbors::Neighbors)
+    surroundings = filter(x->x!=cluster,clusters)
+    for (index,value) in enumerate(surroundings)
+        inter
+
+
+
+
+"""
+get the matrix representation Hamiltonian of a single cluster
 """
 struct ClusterH{L<:AbstractLattice, G<:OperatorSum, M<:OperatorSum} <: Frontend
     cluster::L
