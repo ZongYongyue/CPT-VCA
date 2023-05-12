b01db748ab5837c747fb6348004b5a8e59aafb3f
ZongYongyue <zongyyphy@qq.com>
2023-05-12 17:52:50 +0800

    add new functions

--- src/CPTVCA.jl
+++ src/CPTVCA.jl
@@ -15,6 +15,7 @@ struct Cluster{T<:Number} <: AbstractMatrix{T}
     id::Int
 end
 Base.size(c::Cluster)=size(c.intrasites)
+Base.length(c::Cluster)=size(c.intrasites,2)
 Base.getindex(c::Cluster, i, j) = c.intrasites[i, j]
 Base.setindex!(c::Cluster, v, i, j) = (c.intrasites[i, j] = v)
 function Cluster(lattice::AbstractLattice, id::Int=0)
@@ -50,24 +51,53 @@ function Clusters(cluster::Cluster, steps::Tuple{Int, Vararg{Int}}, vectors::Abs
 end
 
 """
-get the inter-cluster hopping between the given cluster and its surrounding clusters with specific momentum k
+obtain the inter-cluster hopping  between the given cluster and its surrounding clusters with specific momentum k : V^{0,m}(k) 
 """
 function interhopping(clusters::Vector{Cluster}, cluster::Cluster, hilbert::Hilbert, neighbors::Neighbors, k::Vector)
     surroundings = filter(x->x!=cluster,clusters)
     spin = Rational((hilbert[1].nspin-1)/2)
     norbital = hilbert[1].norbital
-    ops = 0
+    ops = Vector{Operator}()
     for (index,value) in enumerate(surroundings), s in -spin:spin, n in 1:norbital
         seqs = interlinks(value, cluster, neighbors)
         if !isempty(seqs)
             for q in 1:length(seqs)
-                op = exp(im*dot(k, value[:,1]))*CompositeIndex(Index(seqs[q][2],FID{:f}(n, s, 2)), cluster[:,seqs[q][2]], [0.0, 0.0])*CompositeIndex(Index(seqs[q][3],FID{:f}(n, s, 1)), cluster[:,seqs[q][3]], [0.0, 0.0])
-                ops += op + op'
+                op = exp(-im*dot(k, value[:,1]))*CompositeIndex(Index(seqs[q][2],FID{:f}(n, s, 2)), cluster[:,seqs[q][2]], [0.0, 0.0])*CompositeIndex(Index(seqs[q][3],FID{:f}(n, s, 1)), cluster[:,seqs[q][3]], [0.0, 0.0])
+                ops = push!(ops, op, op')
             end
         end
     end
-    return ops
+    ops_dict = Dict{Tuple{Int,Int}, Vector{Operator}}()
+    hopping = Dict{Tuple{Int,Int}, Vector{Operator}}()
+    getsites(op::Operator)=(op.id[1].index.site,op.id[2].index.site)
+    for op in ops
+        key = getsites(op)
+        if !haskey(ops_dict, key)
+            ops_dict[key] = []
+        end
+        push!(ops_dict[key], op)
+    end
+    for i = 1:length(cluster), j = 1:length(cluster)
+        key = (i, j)
+        hopping[key] = []
+    end
+    for key in keys(ops_dict)
+        if haskey(hopping, key)
+            hopping[key] = ops_dict[key]
+        end
+    end
+    return hopping
 end
+
+"""
+obtain the inverse cluster Green function with specific frequence ω : G⁻¹(ω)
+"""
+function invCGF(cluster::Cluster, bases::BinaryBases, hilbert::Hilbert, neighbors::Neighbors, ω::Number)
+    
+
+"""
+obtain the CPT Green function with a periodic scheme : G    
+"""
 
 
 
