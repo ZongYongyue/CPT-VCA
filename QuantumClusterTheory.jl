eef92ce4b8d7b6a6ac3eb4bf347bfb27c183114f
ZongYongyue <zongyyphy@qq.com>
2023-07-05 16:50:26 +0800

    fix bug

--- QuantumClusterTheory.jl
+++ QuantumClusterTheory.jl
@@ -1,14 +1,63 @@
-using QuantumLattices
+module QuantumClusterTheory
+
+using Arpack
 using ExactDiagonalization
 using KrylovKit
 using LinearAlgebra
-using Revise
+using QuantumLattices
 
+export GreenFunction, InitialState, FInitialState, SInitialState, Kryvals, LehmannGreenFunction, Sysvals
+export initialstate, clusterGreenFunction
 
+"""
+    abstract type GreenFunction
+"""
 abstract type GreenFunction end
 
-abstract type OnsiteStartStates end
+"""
+    abstract type InitialState
 
+The Operators needed in calculating the initial vector in Lanczos iteration method of krylov subspace
+"""
+abstract type InitialState end
+
+"""
+    FInitialState{O<:Operator} <: InitialState
+    SInitialState{O<:Operator} <: InitialState
+
+The Operators in a fermion/spin system needed in calculating the initial vector in Lanczos iteration method of krylov subspace 
+"""
+struct FInitialState{O<:Operator} <: InitialState
+    sign::Int
+    ops::OperatorSum{O}
+    function FInitialState(sign::Int, key::Tuple)
+        ops = Operators(1*CompositeIndex(Index(key[2], FID{:f}(key[3], key[1], sign)), [0.0, 0.0], [0.0, 0.0]))
+        new{eltype(ops)}(sign, ops)
+    end
+end
+struct SInitialState <: InitialState
+    sign::Int
+    key::Tuple
+    function SInitialState(sign::Int, key::Tuple)
+        new(sign,key)
+        #to be update
+    end
+end
+
+"""
+    initialstate(::EDKind{:FED}, sign::Int, key::Tuple) -> FInitialState
+    initialstate(::EDKind{:SED}, sign::Int, key::Tuple) -> SInitialState
+
+Get the Operators needed in calculating the initial vector
+"""
+function initialstate(::EDKind{:FED}, sign::Int, key::Tuple) FInitialState(sign, key) end
+function initialstate(::EDKind{:SED}, sign::Int, key::Tuple) SInitialState(sign, key) end
+
+"""
+    Kryvals{M<:AbstractMatrix, R<:Real, V<:AbstractVector}
+
+The information obtained with the krylov subspace method that needed in calculating the cluster Green function
+"""
 struct Kryvals{M<:AbstractMatrix, R<:Real, V<:AbstractVector}
     tridimatrix::M
     norm::R
@@ -17,10 +66,15 @@ struct Kryvals{M<:AbstractMatrix, R<:Real, V<:AbstractVector}
         krybasis, tridimatrix = genkrylov(matrix, sstate, m)
         projectvector = KrylovKit.project!(zeros(Complex, m), krybasis, sstate)
         norm = √(sstate'*sstate)
-        new{typeof(tridimatrix), typeof(norm), typeof{projectvector}}(tridimatrix, norm, projectvector)
+        new{typeof(tridimatrix), typeof(norm), typeof(projectvector)}(tridimatrix, norm, projectvector)
     end
 end
 
+"""
+    genkrylov(matrix::AbstractMatrix, sstate::AbstractVector, m::Int)
+
+Generate a krylov subspace with Lanczos iteration method
+"""
 function genkrylov(matrix::AbstractMatrix, sstate::AbstractVector, m::Int)
     matrix = Hermitian(matrix)
     orth = KrylovKit.ModifiedGramSchmidt()
@@ -34,89 +88,68 @@ function genkrylov(matrix::AbstractMatrix, sstate::AbstractVector, m::Int)
     return basis_vectors, T
 end
 
+"""
+    LehmannGreenFunction{R<:Real, I<:Int, S<:Kryvals} <: GreenFunction
 
-struct FOnsiteStartStates{I<:Int, O<:OperatorSum} <: OnsiteStartStates
-    id::I
-    contents::Vector{O}
-    function FOnsiteStartStates(id::Int, site::Int, orderkeys::AbstractVector)
-        contents = Vector{OperatorSum{Operator}}()
-        onsitekeys = filter(key -> key[2] == site, orderkeys)
-        for key in onsitekeys
-            ops = Operators(Operator(1, Index(key[2], FID{:f}(key[3], key[1], id))))
-            push!(contents, ops)
-        end
-        new{typeof(id), eltype(contents)}(id, contents)
-    end
-end
-
-struct SOnsiteStartStates{I<:Int, O<:OperatorSum} <: OnsiteStartStates
-    id::I
-    contents::Vector{O}
-end
-
-struct LehmannGreenFunction{R<:Real, I<:Int, S<:Kryvals} 
+The minimum element of a Green function in Lehmann representation
+"""
+struct LehmannGreenFunction{R<:Real, I<:Int, S<:Kryvals} <: GreenFunction
     GSEnergy::R
-    id::I
+    sign::I
     kryvals_l::S
     kryvals_r::S
+    function LehmannGreenFunction(gse::Real, sign::Int, kryvals_l::Kryvals, kryvals_r::Kryvals)
+        new{typeof(gse), typeof(sign), typeof(kryvals_l)}(gse, sign, kryvals_l, kryvals_r)
+    end
 end
-
 function (gf::LehmannGreenFunction)(ω::Complex, μ::Real)
-    Im = Matrix{Complex}(I, length(gf.kryvals_l.tridimatrix), length(gf.kryvals_l.tridimatrix))
-    if gf.id == 1
-        lgf = dot(gf.kryvals_l.projectvector, inv((ω + μ - GSEnergy)*Im + Matrix(gf.kryvals_r.tridimatrix))[:, 1])*gf.kryvals_r.norm
-    else if gf.id == 2
-        lgf = dot(gf.kryvals_l.projectvector, inv((ω + μ + GSEnergy)*Im - Matrix(gf.kryvals_r.tridimatrix))[:, 1])*gf.kryvals_r.norm
+    Im = Matrix{Complex}(I, size(gf.kryvals_l.tridimatrix,2), size(gf.kryvals_l.tridimatrix,2))
+    if gf.sign == 1
+        lgf = dot(gf.kryvals_l.projectvector, inv((ω + μ - gf.GSEnergy)*Im + Matrix(gf.kryvals_r.tridimatrix))[:, 1])*gf.kryvals_r.norm
+    elseif gf.sign == 2
+        lgf = dot(gf.kryvals_l.projectvector, inv((ω + μ + gf.GSEnergy)*Im - Matrix(gf.kryvals_r.tridimatrix))[:, 1])*gf.kryvals_r.norm
     end
     return lgf
 end
 
+"""
+    Sysvals{K<:EDKind, R<:Real, S<:Kryvals}
+
+The all information needed to calculate the Green Function of a finite size system 
+"""
 struct Sysvals{K<:EDKind, R<:Real, S<:Kryvals}
     GSEnergy::R
     setkryvals₁::Vector{S}
     setkryvals₂::Vector{S}
 end
-function Sysvals(k::EDKind{:FED}, gs::AbstractVector, gse::Real, gen::OperatorGenerator, target::TargetSpace, hilbert::Hilbert; m::Int=200) 
+function Sysvals(k::EDKind, gs::AbstractVector, gse::Real, gen::OperatorGenerator, target::TargetSpace, hilbert::Hilbert; m::Int=200)
     table = Table(hilbert, Metric(k, hilbert))
-    setkryvals₁, setkryvals₂ = fill(Vector{Kryvals}(), 2)
+    setkryvals₁, setkryvals₂ = (Vector{Kryvals}(), Vector{Kryvals}())
     H₁, H₂ = matrix(expand(gen), (target[2], target[2]), table), matrix(expand(gen), (target[3], target[3]), table)
     orderkeys = sort(collect(keys(table)), by = x -> table[x])
-    for i in 1:length(hilbert)
-        for ops in FOnsiteStartStates(1, i, orderkeys).contents
-            sstate₁ = (matrix(ops, (target[2], target[1]), table)*gs)[:,1]
-            push!(setkryvals₁, Kryvals(H₁, sstate₁, m))
-        end
-        for ops in FOnsiteStartStates(2, i, orderkeys).contents
-            sstate₂ = (matrix(ops, (target[3], target[1]), table)*gs)[:,1]
-            push!(setkryvals₂, Kryvals(H₂, sstate₂, m))
-        end
+    for key in orderkeys
+        ops₁, ops₂ = initialstate(k, 1, key).ops, initialstate(k, 2, key).ops
+        sstate₁, sstate₂ = (matrix(ops₁, (target[2], target[1]), table)*gs)[:,1], (matrix(ops₂, (target[3], target[1]), table)*gs)[:,1]
+        push!(setkryvals₁, Kryvals(H₁, sstate₁, m))
+        push!(setkryvals₂, Kryvals(H₂, sstate₂, m))
     end
-    return Sysvals{typeof(k), typeof{gse}, eltype{setkryvals₁}}(gse, setkryvals₁, setkryvals₂)
+    return Sysvals{typeof(k), typeof(gse), eltype(setkryvals₁)}(gse, setkryvals₁, setkryvals₂)
 end
 
-function Sysvals(k::EDKind{:SED}, gs::AbstractVector, gse::Real, gen::OperatorGenerator, target::TargetSpace, hilbert::Hilbert; m::Int=200) 
-end
+"""
+    clusterGreenFunction(sys::Sysvals, ω::Complex, μ::Real)
 
-function ClusterGreenFunction(sys::Sysvals, ω::Complex, μ::Real)
+Calculate the cluster Green function
+"""
+function clusterGreenFunction(sys::Sysvals, ω::Complex, μ::Real)
     cgf = zeros(Complex, length(sys.setkryvals₁), length(sys.setkryvals₁))
     for i in 1:length(sys.setkryvals₁), j in 1:length(sys.setkryvals₁)
-        sys.setkryvals₁[i], sys.setkryvals₁[j], sys.setkryvals₂[i], sys.setkryvals₂[j]
-        gf₂ = LehmannGreenFunction(gse, 2, sys.setkryvals₂[i], sys.setkryvals₂[j])
-        gf₁ = LehmannGreenFunction(gse, 1, sys.setkryvals₁[j], sys.setkryvals₁[i])
+        gf₂ = LehmannGreenFunction(sys.GSEnergy, 2, sys.setkryvals₂[i], sys.setkryvals₂[j])
+        gf₁ = LehmannGreenFunction(sys.GSEnergy, 1, sys.setkryvals₁[j], sys.setkryvals₁[i])
         cgf[i, j] += gf₂(ω, μ) + gf₁(ω, μ)
     end
     return cgf
 end
 
 
-
-
-
-hilbert_F = Hilbert(site=>Fock{:f}(1, 2) for site=1:2)
-hilbert_S = Hilbert(site=>Spin{1//2}() for site=1:2)
-st = Table(hilbert_S)
-ft = Table(hilbert_F, Metric(EDKind{:FED}(),hilbert_F))
-
-orderkeys = sort(collect(keys(ft)), by = x -> ft[x])
-a = FOnsiteStartStates(1,1,orderkeys)
-
+end # module
